00194d957332635b9e722093d03fe0f65050c966
PranavSandeep <pranavsandeep21@gmail.com>
2021-02-24 21:17:51 +0530

    Commit 2

--- Powerpoint Writer.py
+++ Powerpoint Writer.py
@@ -2,9 +2,35 @@ from pptx import Presentation
 from pptx.util import Inches
 import wikipedia
 import os
-import Gui
 import urllib.request
 import random
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+
+root = tk.Tk()
+root.title('Azure')
+
+window_height = 530
+window_width = 800
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x_cordinate = int((screen_width / 2) - (window_width / 2))
+y_cordinate = int((screen_height / 2) - (window_height / 2))
+
+root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+
+style = ttk.Style(root)
+root.tk.call('source', 'azure dark.tcl')
+style.theme_use('azure')
+
+button = Button(root, text="DELET", command=root.destroy)
+
+button.pack()
+
+root.mainloop()
 
 prs = Presentation()
 blank_slide_layout = prs.slide_layouts[0]
@@ -72,6 +98,29 @@ except wikipedia.exceptions.DisambiguationError as e:
 
 
 
+root = tk.Tk()
+root.title('Azure')
+
+window_height = 530
+window_width = 800
+
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+x_cordinate = int((screen_width / 2) - (window_width / 2))
+y_cordinate = int((screen_height / 2) - (window_height / 2))
+
+root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+
+style = ttk.Style(root)
+root.tk.call('source', 'azure dark.tcl')
+style.theme_use('azure')
+
+button = Button(root, text="DELET", command=root.destroy)
+
+button.pack()
+
+root.mainloop()
 
 prs.save('Englishact.ppt')
 os.startfile('Englishact.ppt')
